9521bc69b3791f3720cf318a821aac4e29b75065
mfjv88 <mfjvacas@gmail.com>
2016-04-19 11:30:04 -0400

    Battle system and end level work

--- assets/js/finalproject.js
+++ assets/js/finalproject.js
@@ -1,6 +1,5 @@
 /*This is a Text based RPG Game*/
 
-// A $( document ).ready() block.
 $( document ).ready(function() {
     $('#attack').hide();
     $('#flee').hide();
@@ -19,19 +18,30 @@ var area = [
 
 currentMonster = null;
 turn = null;
-goblin = {
+bossDefeated = false;
+
+goblin1 = {
   rep: 2,
   hp: 5,
   attack: 2,
   defense: 0,
-  currentPosition: [{x: 1, y: 1, id: 1}, {x: 4, y: 2, id:2}]
+  currentPosition: {x: 1, y: 1, id: 1}
+};
+
+goblin2 = {
+  rep: 2,
+  hp: 5,
+  attack: 2,
+  defense: 0,
+  currentPosition: {x: 4, y: 2, id:2}
 };
 
 orc = {
   rep: 3,
   hp: 10,
   attack: 4,
-  defense: 2
+  defense: 2,
+  currentPosition: {x: 3, y: 0, id:3}
 };
 
 hero = {
@@ -50,18 +60,20 @@ var walk = {
   rep: 1
 };
 
+treasure = {
+  rep: 5
+};
+
 
 /*All the functions within the game are being defined in this section*/
-
-// var heroDamage = this.hp -= (hero.attack - this.defense);
-
-// var monsterDamage = hero.hp -= (this.attack - hero.defense);
-
-/*Work in progress*/
 var battle = function() {
   console.log(currentRep, currentMonster);
   if (currentRep === 2) {
-    currentMonster = goblin;
+    if ((hero.currentPosition.x === 1 && hero.currentPosition.y === 2) || (hero.currentPosition.x === 2 && hero.currentPosition.y === 1)) {
+      currentMonster = goblin1;
+    } else {
+      currentMonster = goblin2;
+    }
   } else {
     currentMonster = orc;
   }
@@ -77,9 +89,28 @@ var battle = function() {
       $('#left').show();
       $('#down').show();
       $('#right').show();
-      area[currentMonster.currentPosition.y][currentMonster.currentPosition.x] = 1;
-      $('#tile'+currentMonster.currentPosition.y+currentMonster.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
-      ;
+      switch (currentMonster) {
+        case goblin1:
+          console.log(goblin1)
+          area[goblin1.currentPosition.y][goblin1.currentPosition.x] = 1;
+          $('#tile'+ goblin1.currentPosition.y + goblin1.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
+          ;
+          break;
+        case goblin2:
+          console.log(goblin2)
+          area[goblin2.currentPosition.y][goblin2.currentPosition.x] = 1;
+          $('#tile'+ goblin2.currentPosition.y + goblin2.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
+          ;
+          break;
+        case orc:
+          area[orc.currentPosition.y][orc.currentPosition.x] = 5;
+          console.log(area[0][3]);
+          bossDefeated = true;
+          $('#tile03').html("<img src='assets\\images\\chest.png' id='chest' alt='chest' width='40px'/>");
+          break;
+        default:
+          break;
+      }
       break;
     } else if(hero.hp < 1) {
       $("#prompt").append("You died" + "<br>");
@@ -90,35 +121,6 @@ var battle = function() {
     break;
   }
 };
-
-
-
-/*
-  while (battle === true) {
-    switch (battle_options) {
-      case "attack":
-        heroDamage;
-        $("#prompt").append("You hit the monster and did " + heroDamage + " damage!" + "<br>");
-        if (this.hp === 0) {
-          $("#prompt").append("You defeated the monster!" + "<br>")
-        } else {
-          monsterDamage;
-          if (hero.hp >= 0) {
-            battle();
-          } else {
-            $("#prompt").append("The monster defeated you!" + "<br>");
-            battle === false;
-          }
-        };
-        break;
-      case "run":
-        flee();
-        break;
-      default:
-        $("#prompt").append("This is not a command! Try again!" + "<br>");
-    };
-  };
-};*/
 
 var checkArea = function(x, y) {
 
@@ -138,14 +140,17 @@ var checkArea = function(x, y) {
     $('#right').hide();
     currentMonster = { x: x, y: y};
     return false;
-    // if (confirm("Do you want to battle?" === true)) {
-      // battle();
-    // } else {
-       // console.log("Choose your next action");
-    //   action();
-    // }
+  } else if (currentRep === 5) {
+    $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
+    $('#tile'+'0'+hero.currentPosition.x).html("<img src='assets\\images\\linktriforce.png' alt='treasure' width='40px'/>")
+    $("#prompt").append("You find the treasure!" + "<br>");
+    $("#prompt").append("You passed the level!" + "<br>");
+    $('#up').hide();
+    $('#left').hide();
+    $('#down').hide();
+    $('#right').hide();
+    return true;
   } else {
-    // console.log("This command doesn't exist. Try again!")
     $("#prompt").append("Can't go there fool!" + "<br>");
     return false;
   };
@@ -172,22 +177,21 @@ move = function(move) {
   var temp = null;
   switch (move) {
     case "up":
-      if (hero.currentPosition.y-1 > 4 || hero.currentPosition.y-1 < 0) {
+      if (hero.currentPosition.y-1 > 5 || hero.currentPosition.y-1 < 0) {
         $("#prompt").append("Can't go there fool!" + "<br>");
         return false;
       }
       var canMove = checkArea(hero.currentPosition.x,   hero.currentPosition.y-1);
-      console.log(hero);
       if (canMove) {
-        area[hero.currentPosition.y][hero.currentPosition.x] = 1;
-        $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
-        hero.currentPosition.y -= 1;
-        area[hero.currentPosition.y][hero.currentPosition.x] = 4;
-        $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\link.gif' alt='hero' width='40px'/>");
-        console.log(area[hero.currentPosition.y][hero.currentPosition.x], area[hero.currentPosition.y][hero.currentPosition.x]);
+        if (!bossDefeated) {
+          area[hero.currentPosition.y][hero.currentPosition.x] = 1;
+          $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
+          hero.currentPosition.y -= 1;
+          area[hero.currentPosition.y][hero.currentPosition.x] = 4;
+          $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\link.gif' alt='hero' width='40px'/>");
+        } else {
+        }
       } else {
-        // $("#prompt").append("Can't do that fool" + "<br>");
-        //$('#propt').append('')
       }
       break;
     case "down":
@@ -196,14 +200,12 @@ move = function(move) {
         return false;
       }
       var canMove = checkArea(hero.currentPosition.x,   hero.currentPosition.y+1);
-      console.log(hero);
       if (canMove) {
         area[hero.currentPosition.y][hero.currentPosition.x] = 1;
         $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
         hero.currentPosition.y += 1;
         area[hero.currentPosition.y][hero.currentPosition.x] = 4;
         $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\link.gif' alt='hero' width='40px'/>");
-        console.log(area[hero.currentPosition.y][hero.currentPosition.x], area[hero.currentPosition.y][hero.currentPosition.x]);
       } else {
         //$("#prompt").append("Can't do that fool!"  + "<br>");
         //$('#propt').append('')
@@ -215,14 +217,12 @@ move = function(move) {
         return false;
       }
       var canMove = checkArea(hero.currentPosition.x-1,   hero.currentPosition.y);
-      console.log(hero);
       if (canMove) {
         area[hero.currentPosition.y][hero.currentPosition.x] = 1;
         $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
         hero.currentPosition.x -= 1;
         area[hero.currentPosition.y][hero.currentPosition.x] = 4;
         $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\link.gif' alt='hero' width='40px'/>");
-        console.log(area[hero.currentPosition.y][hero.currentPosition.x], area[hero.currentPosition.y][hero.currentPosition.x]);
       } else {
         // $("#prompt").append('cant do that fool' + "<br>");
         //$('#propt').append('')
@@ -234,14 +234,12 @@ move = function(move) {
         return false;
       }
       var canMove = checkArea(hero.currentPosition.x+1,   hero.currentPosition.y);
-      console.log(hero);
       if (canMove) {
         area[hero.currentPosition.y][hero.currentPosition.x] = 1;
         $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\floor.png' alt='tile' width='40px'/>");
         hero.currentPosition.x += 1;
         area[hero.currentPosition.y][hero.currentPosition.x] = 4;
         $('#tile'+hero.currentPosition.y+hero.currentPosition.x).html("<img src='assets\\images\\link.gif' alt='hero' width='40px'/>");
-        console.log(area[hero.currentPosition.y][hero.currentPosition.x], area[hero.currentPosition.y][hero.currentPosition.x]);
       } else {
         //$("#promp").append('cant do that fool' + "<br>");
         //$('#propt').append('')
